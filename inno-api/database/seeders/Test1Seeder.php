0b69bd3cd4f8289c946bdb0e0437704334fb1941
Dmitry <55439124+TTepel7@users.noreply.github.com>
2021-12-03 17:13:11 +0500

    generator create

---
+++ inno-api/database/seeders/Test1Seeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Pilot;
+use App\Models\Startup;
+use App\Models\Team;
+use App\Models\Team_member;
+use Illuminate\Database\Seeder;
+
+class Test1Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Team_member::factory(80)->create();
+        Team::factory(45)->create();
+        Startup::factory(120)->create()->each(function ($startup){
+            $startup->pilots()->saveMany(Pilot::factory(rand(1,3))->make());
+        });
+    }
+}
