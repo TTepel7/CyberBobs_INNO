84214cb85aa9240cc83da9a94219e7035776146a
Dmitry <55439124+TTepel7@users.noreply.github.com>
2021-12-03 15:31:55 +0500

    add resurces

--- inno-api/app/Http/Middleware/Authenticate.php
+++ inno-api/app/Http/Middleware/Authenticate.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\UnauthorizedException;
 
 class Authenticate extends Middleware
 {
@@ -16,8 +19,17 @@ class Authenticate extends Middleware
     {
         if (! $request->expectsJson()) {
             return route('login');
-        return false;
         }
 
+    }
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if (Auth::guest()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        // other checks
+
+        return $next($request);
     }
 }
--- inno-api/routes/web.php
+++ inno-api/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('http://innocase.cyberbobs.xsph.ru/login');
+    return response()->json(['error' => 'Wrong Login or Password.'], 401);
 })->name('login');
