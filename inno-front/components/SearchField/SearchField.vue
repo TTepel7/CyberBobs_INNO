66e5de54834dcc83513991eb7addf1c7cb76b393
Alex Ebenrode <alexebenrode@gmail.com>
2021-12-03 12:03:10 +0500

    add search field

---
+++ inno-front/components/SearchField/SearchField.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="root">
+    <div class="content">
+      <img class="icon" :src="require('./search.svg')"/>
+      <input
+        class="input"
+        type="text"
+        v-bind="$attrs"
+        @input="$emit('update', $event.target.value)"
+        v-on="$listeners"
+      />
+    </div>
+    <button class="button">Найти идеальное решение</button>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'text',
+    event: 'update',
+  },
+  props:{
+    text: {
+      type: String,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.root{
+  display: flex;
+  justify-content: space-between;
+  border-radius: 48px;
+  box-shadow: 0px 10px 10px 2px rgba(0, 0, 0, 0.05);
+
+}
+
+.icon{
+  margin-right: 14px;
+}
+
+.input {
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 29px;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+.content{
+  display:flex;
+  margin-left: 20px;
+}
+
+.button{
+  height: 100%;
+  padding: 20px 42px;
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 29px;
+  background-color: #009A96;
+  border-radius: 48px;
+}
+
+</style>
--- inno-front/pages/index.vue
+++ inno-front/pages/index.vue
@@ -1,23 +1,30 @@
 <template>
   <div>
-    Привет мир!<button @click='logout'>Выйти</button>
-    {{ userData }}
+    <button @click='logout'>Выйти</button>
+    <SearchField v-model="searchText"
+        placeholder="Начинайте вводить..." />
+    {{ searchText }}
   </div>
 </template>
 
 <script>
 import { logout, getUserProfile } from '~/api/api';
+import SearchField from '~/components/SearchField/SearchField.vue';
 
 export default {
+  components: {
+    SearchField
+  },
   data(){
     return {
       userData: {},
+      searchText: '',
     }
   },
   head(){
     return {
-        title: 'Витрина',
-      }
+      title: 'Витрина',
+    }
   },
   async fetch(){
     this.userData = await getUserProfile();
