f2ee12260f16d25d078e47d46acffc8d574a9d58
Dmitry <55439124+TTepel7@users.noreply.github.com>
2021-12-04 00:40:40 +0500

    add anonim

--- inno-api/app/Models/Team_member.php
+++ inno-api/app/Models/Team_member.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team_member extends Model
 {
+    private function anonymize($str)
+    {
+        return substr_replace(substr_replace($str, '****', 0, 4), '****', -4);
+    }
+
     use HasFactory;
+
+    public function getPhoneAttribute($value)
+    {
+        return $this->anonymize($this->attributes['phone']);
+    }
+
+    public function getLastNameAttribute($value)
+    {
+        return $this->anonymize($this->attributes['last_name']);
+    }
+
+    public function getEmailAttribute($value)
+    {
+        return $this->anonymize($this->attributes['email']);
+    }
 }
